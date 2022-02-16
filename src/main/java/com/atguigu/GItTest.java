cc2f160e88119a62e4aad448c5743d05b2b6a703
oyd588 <1286324547@qq.com>
2022-02-16 11:24:36 +0800

    hot commit

--- src/main/java/com/atguigu/GItTest.java
+++ src/main/java/com/atguigu/GItTest.java
@@ -3,7 +3,8 @@ package com.atguigu;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("GitTest");
-        System.out.println("GitTest222");
-        System.out.println("GitTest333");
+        System.out.println("GitTest");
+        System.out.println("GitTest");
+        System.out.println("GitTest");
     }
 }
