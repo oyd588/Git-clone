890ac6b61c609718fdd528dae95e9428f29b11e9
oyd588 <1286324547@qq.com>
2022-02-16 10:14:56 +0800

    second commit

--- src/main/java/com/atguigu/GItTest.java
+++ src/main/java/com/atguigu/GItTest.java
@@ -3,5 +3,6 @@ package com.atguigu;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("GitTest");
+        System.out.println("GitTest222");
     }
 }
